50afed65d32f5f2a5beba7e7f2644929ea33a258
Gabriel Meghnagi <gabrielmeghnagi@outlook.it>
2020-04-26 02:32:39 +0200

    some changes

--- vc/auth/commands.py
+++ vc/auth/commands.py
@@ -6,17 +6,16 @@ import yaml
 from vc.helpers.context_helper import get_unverified_context, random_string, load_config, dump_config
 
 
-
 @click.command()
 @click.option('--vcenter', '-v', 
               help='vcenter you want to conntect to.',
-              required=True)
+              required=False)
 @click.option('--username', '-u',
               help='username to connect to vcenter.',
-              required=True)
+              required=False)
 @click.option('--password', '-p',
               help='password to connect to vcenter.',
-              required=True)  
+              required=False)  
 def auth(vcenter, username, password):
     try:
         si = SmartConnect(host=vcenter, 
@@ -28,11 +27,17 @@ def auth(vcenter, username, password):
                                'username': username, 
                                'token': si._stub.cookie},
                    'name': context_name}
-        config = load_config()
-        config['contexts'].append(context)
-        config['current-context'] = context['name']
-        dump_config(config)
-        return
+        try: 
+            config = load_config()
+            config['contexts'].append(context)
+            config['current-context'] = context['name']
+            dump_config(config)
+            return
+        except FileNotFoundError:
+            config = {'contexts': [context], 
+                      'current-context': context_name}
+            dump_config(config)
+            return
     except Exception as e:
         print(e)
         return
