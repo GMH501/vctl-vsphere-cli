97ab5812bc379aaf27191b82ce515649f0cc29a8
GMH501 <gabrielmeghnagi@outlook.it>
2020-06-22 20:36:49 +0200

    with_vars, loop optimized + minors

--- vctl/run/cmd.py
+++ vctl/run/cmd.py
@@ -24,11 +24,6 @@ except:
 
 
 init(convert=True)
-REGEX = re.compile('\$item')
-
-
-def loop():
-    pass
 
 
 def load_yaml(path):
@@ -36,7 +31,7 @@ def load_yaml(path):
         with open(path, 'r') as config_file:
             return list(yaml.safe_load_all(config_file))
     except FileNotFoundError:
-        print('Config file not found.')
+        print('Runner not found in path: {}'.format(path))
         raise SystemExit(1)
 
 
@@ -95,7 +90,7 @@ def snapshot(content, mode, vm, description, memory=False, quiesce=False, wait=T
                 return 2, None
 
         else:
-            error ='Incorrect parameter: {}'.format(mode)
+            error ='Incorrect parameter: {} for function snapshot'.format(mode)
             raise SystemExit(error)
 
     except vim.fault.NotAuthenticated:
@@ -109,6 +104,63 @@ def snapshot(content, mode, vm, description, memory=False, quiesce=False, wait=T
         return 1, error
 
 
+def with_vars(function, variables):
+    FIND_VAR = re.compile('\$var.([\\w]*)')
+    ### LA FUNZIONE SUPPORTA UN SOLO REPLACE PER STRINGA
+    new_parameters = dict()
+    for argument, parameter in function.items():
+        if type(parameter) == str and parameter.count('$var.') == 1:
+            try:
+                key = re.findall(FIND_VAR, parameter)[0]
+                variable = variables[key]
+                REGEX_VAR = eval("re.compile(r'\$var\.{}')".format(key))
+                if type(variable) == str:
+                    new_param = re.sub(REGEX_VAR, variable, parameter)
+                    new_parameters[argument] = new_param
+                elif type(variable) == int:
+                    new_param = re.sub(REGEX_VAR, str(variable), parameter)
+                    new_parameters[argument] = int(new_param) \
+                        if new_param.isdecimal() else new_param
+                elif type(variable) == bool:
+                    new_parameters[argument] = variable
+            except KeyError:  ## se all'interno di un parameter trova una $var.<qualcosa> non conosciuta
+                new_parameters[argument] = parameter
+        elif type(parameter) == str and parameter.count('$var.') > 1:
+            keys = re.findall(FIND_VAR, parameter)
+            new_param = parameter
+            for key in keys:
+                if key in variables:
+                    variable = variables[key]
+                    REGEX_VAR = eval("re.compile(r'\$var\.{}')".format(key))
+                    new_param = re.sub(REGEX_VAR, str(variable), new_param)
+                else:
+                    pass
+            new_parameters[argument] = new_param
+        else:
+            new_parameters[argument] = parameter
+    return new_parameters
+
+
+def loop(function, item):
+    LOOP_REGEX = re.compile('\$item')
+    parameters = dict()
+    for argument, parameter in function.items():
+        if type(parameter) == str:
+            subs_param = re.sub(LOOP_REGEX, str(item), parameter)
+            parameters[argument] = subs_param
+        else:
+            parameters[argument] = parameter
+    return parameters
+
+
+def func_output(rc, log):
+    if rc == 0:
+        print('{}  rc => {}, log => {}{}'.format(Fore.YELLOW, rc, log, Fore.RESET))
+    elif rc == 1:
+        print('{}  rc => {}, log => {}{}'.format(Fore.RED, rc, log, Fore.RESET))
+    elif rc == 2:
+        pass
+
 @click.command()
 @click.option('--context', '-c',
               help='The context to use for run the command, the default is <current-context>.',
@@ -121,44 +173,32 @@ def run(file, context):
         si = inject_token(context)
         content = si.content
         file_path = get_path(file)
-        tasks = load_yaml(file_path)
-        for task in tasks:
-            task_keys = list(task)
-            if 'name' in task_keys:
-                print('{}===> [{}]'.format(Fore.RESET, task['name']))
-                task_keys.remove('name')
-            for key in task_keys:
-                if (key not in dir(sys.modules[__name__])) and \
-                    key not in ['loop']:
-                    raise ValueError('{}function {} not defined.'.format(Fore.RESET, key))
-            for key in task_keys:
-                if key in ['loop']:
+        documents = load_yaml(file_path)
+        for document in documents:
+            functions = list(document)
+            if 'name' in functions:
+                print('===> [{}]'.format(document['name']))
+                functions.remove('name')
+            for function in functions:
+                if not eval('callable({})'.format(function)):
+                    raise ValueError('function {} not defined.'.format(function))
+            for function in functions:
+                if function in ['loop', 'with_vars']:
                     continue
-                function = key
-                if 'loop' in task_keys:
-                    for item in task['loop']:
-                        parameters = dict()
-                        for argument, parameter in task[function].items():
-                            if type(parameter) == str:
-                                subs_param = re.sub(REGEX, item, parameter)
-                                parameters[argument] = subs_param
-                            else:
-                                parameters[argument] = parameter
-                        rc, log = eval(function + "(content=content, **parameters)")
-                        if rc == 0:
-                            print('{}  rc => {}, log => {}'.format(Fore.YELLOW, rc, log))
-                        elif rc == 1:
-                            print('{}  rc => {}, log => {}'.format(Fore.RED, rc, log))
-                        elif rc == 2:
-                            pass
+                func_params = document[function]
+                if 'with_vars' in functions:
+                    for variables in document['with_vars']:
+                        new_params = with_vars(func_params, variables)
+                        rc, log = eval(function + "(content=content, **new_params)")
+                        func_output(rc, log)   
+                elif 'loop' in functions:
+                    for item in document['loop']:
+                        new_params = loop(func_params, item)
+                        rc, log = eval(function + "(content=content, **new_params)")
+                        func_output(rc, log)
                 else:
-                    rc, log = eval(function + "(content=content, **task[function])")
-                    if rc == 0:   # wait success
-                        print('{}  rc => {}, log => {}'.format(Fore.GREEN, rc, log))
-                    elif rc == 1: # wait error
-                        print('{}  rc => {}, log => {}'.format(Fore.RED, rc, log))
-                    elif rc == 2: # no wait
-                        print('{}  rc => {}, log => {}'.format(Fore.RED, rc, log))
+                    rc, log = eval(function + "(content=content, **func_params)")
+                    func_output(rc, log)
 
     except Exception as e:
         print('{}Caught error: {}'.format(Fore.RESET, e))
